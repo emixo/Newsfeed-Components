c02fbc3529b54136a04750da2bd1c66f8346fe18
Emilio <ediaz-goico@me.bergen.edu>
2020-04-09 11:55:04 -0400

    finished MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,27 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+
+const menu = (menuItems) => {
+  const menu = document.createElement("div");
+  const menuList = document.createElement("ul");
+
+  menu.appendChild(menuList);
+
+  menuItems.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    menuList.appendChild(listItem);
+  });
+
+  const menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", (event) => {
+    menu.classList.toggle("menu--open");
+  });
+  return menu;
+};
+
+const header = document.querySelector(".header");
+
+header.prepend(menu(menuItems));
